e7d3de47f775d459d2d1e10363ec975a82a12f83
lawraisins <97999866+lawraisins@users.noreply.github.com>
2024-02-06 23:09:34 +0800

    View following/follower count of other users

--- components/otheruser.js
+++ components/otheruser.js
@@ -31,21 +31,23 @@ const OtherUser = ({ userId, refreshing, onRefresh }) => {
   const [username, setUsername] = useState("DefaultUsername");
   const [handle, setHandle] = useState("farmer");
   const [bio, setBio] = useState("");
+  const [followers, setFollowers] = useState(0);
+  const [following, setFollowing] = useState(0);
   const [fetchInterval, setFetchInterval] = useState(null);
 
   useEffect(() => {
     fetchProfile();
-    // fetchFollowingCount();
-    // fetchFollowerCount();
-  }, [fetchProfile]);
+    fetchFollowingCount();
+    fetchFollowerCount();
+  }, [fetchProfile, fetchFollowingCount, fetchFollowerCount]);
 
   const updateProfile = React.useCallback(() => {
     setFetchInterval(setInterval(() => {
       fetchProfile();
-      // fetchFollowingCount();
-      // fetchFollowerCount();
-    }, 60000)); // fetch every minute
-  }, [fetchProfile]);
+      fetchFollowingCount();
+      fetchFollowerCount();
+    }, 5000)); // fetch every 5 seconds
+  }, [fetchProfile, fetchFollowingCount, fetchFollowerCount]);
 
   useEffect(() => {
     updateProfile();
@@ -90,6 +92,65 @@ const OtherUser = ({ userId, refreshing, onRefresh }) => {
     }
   };
 
+  const fetchFollowingCount = React.useCallback(async () => {
+    try {
+      const token = await _getToken();
+      const response = await fetch(`${backendURL}/users/otherFollowingCount`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+        },
+        body: JSON.stringify({ id }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Update the state with the retrieved profile data
+        try {
+          console.log(data.results[0]['COUNT(*)'])
+          setFollowing(data.results[0]['COUNT(*)'])
+          // You can use the username and bio values here as needed
+        } catch (error) {
+          console.error("Error retrieving following count:", error);
+        }
+      } else {
+        console.error('Failed to retrieve following count:', data.error);
+      }
+    } catch (error) {
+      console.error('Error fetching following count:', error.message);
+    }
+  }, []);
+
+
+  const fetchFollowerCount = React.useCallback(async () => {
+    try {
+      const token = await _getToken();
+      const response = await fetch(`${backendURL}/users/otherFollowerCount`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+        },
+        body: JSON.stringify({ id }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        // Update the state with the retrieved profile data
+        try {
+          console.log(data.results[0]['COUNT(*)'])
+          setFollowers(data.results[0]['COUNT(*)'])
+          // You can use the username and bio values here as needed
+        } catch (error) {
+          console.error("Error retrieving follower count:", error);
+        }
+      } else {
+        console.error('Failed to retrieve follower count:', data.error);
+      }
+    } catch (error) {
+      console.error('Error fetching follower count:', error.message);
+    }
+  }, []);
+
   // Need to fetch bio from Users Table using axios, populate bio section
   // Create an edit user section that can save the changes
   // Allow users to use camera along with uploading from gallery
@@ -99,9 +160,11 @@ const OtherUser = ({ userId, refreshing, onRefresh }) => {
     <View style={styles.container} onRefresh={onRefresh} refreshing={refreshing}>
       <Image style={styles.dp} source={Logo}></Image>
       <View style={styles.info}>
-        <Text style={styles.header}>{username}</Text>
+      <Text style={styles.header}>{username}</Text>
         <Text style={styles.subheader}>@{handle}</Text>
         <Text style={styles.subheader}>{bio}</Text>
+        <Text>Followers:{followers}</Text>
+        <Text>Following:{following}</Text>
       </View>
     </View>
   );
