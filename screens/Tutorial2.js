fdb51f615d6b71810e4f5bed537d46e9579ca0a0
lawraisins <97999866+lawraisins@users.noreply.github.com>
2024-04-12 00:25:49 +0800

    Video Headers

--- screens/Tutorial2.js
+++ screens/Tutorial2.js
@@ -1,5 +1,5 @@
 import React, { useState, useCallback, useRef } from "react";
-import { Button, View, Alert } from "react-native";
+import { Button, View, Alert, Text, StyleSheet } from "react-native";
 // import {Button, StyleSheet, Text, View, Image, TouchableOpacity} from 'react-native';
 import YoutubePlayer from "react-native-youtube-iframe";
 
@@ -23,8 +23,9 @@ const togglePlaying = useCallback(() => {
 
 return (
   <View>
+        <Text style={styles.header}>Triogro App Soil Building Test</Text>
     <YoutubePlayer
-      height={300}
+      height={500}
       play={playing}
       videoId={"IRdP04ClG_k"}
       onChangeState={onStateChange}
@@ -32,4 +33,56 @@ return (
   </View>
 );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FAF4E6",
+    padding: 20,
+    // justifyContent: "center"
+  },
+  headingBox: {
+    // paddingVertical: 10,
+    borderRadius: 10,
+    // marginBottom: 10,
+
+  },
+  heading: {
+    fontSize: 20,
+    fontFamily: 'Arial-Rounded',
+    color: "#4B2209",
+  },
+  tutorialBox: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 20,
+    justifyContent: "center",
+    marginBottom: 16,
+  },
+  tutorialTitle: {
+    fontSize: 14,
+    fontFamily: 'Arial-Rounded',
+    color: "#4B2209",
+    marginBottom: 0,
+  },
+  greenBox: {
+    backgroundColor: '#A0D29F',
+    padding: 20,
+    justifyContent: "center",
+    borderRadius: 10,
+    marginBottom: 10,
+  },
+  tutorialContentBox: {
+    // marginTop: 10,
+    padding: 10,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+  },
+  header: {
+    fontSize: 35,
+    fontFamily: "Arial-Rounded",
+    color:"#004F18",
+
+  },
+});
+
 export default Tutorial2
